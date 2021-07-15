f8a7fde083eb4c82b35fa6885bb4eeaf1437c59e
gokulnair2001 <gokulnair.2001@gmail.com>
2021-07-15 19:16:01 +0530

    Initial commit

---
+++ Example/Floaton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Floaton
+//
+//  Created by gokulnair2001 on 07/15/2021.
+//  Copyright (c) 2021 gokulnair2001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
